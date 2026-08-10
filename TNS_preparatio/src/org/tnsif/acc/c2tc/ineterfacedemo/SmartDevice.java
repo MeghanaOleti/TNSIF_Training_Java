bbc7595b89662f28673ec14bb344dc54bd11d779
oletimeghana <oletimeghana827@gmail.com>
2026-08-10 10:44:54 +0530

    first commit

---
+++ TNS_preparatio/src/org/tnsif/acc/c2tc/ineterfacedemo/SmartDevice.java
@@ -0,0 +1,12 @@
+package org.tnsif.acc.c2tc.ineterfacedemo;
+
+public interface SmartDevice {
+	
+	public abstract void turnOn();
+	public abstract  void turnOff();
+	public abstract  void getStatus();
+	
+	
+	
+
+}
